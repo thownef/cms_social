5746a9c4a203713e1dc9ce71d43bee19c1c520b6
VJD - Tho Nguyen <tho.nguyen@vj-partner.com>
2024-12-30 16:35:02 +0700

    update folder

---
+++ app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Actions\Profile\ShowAction;
+use App\Actions\Profile\UpdateAction;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Profile\UpdateRequest;
+use App\Models\Profile;
+use Illuminate\Http\JsonResponse;
+
+class ProfileController extends Controller
+{
+    /**
+     * @param Profile $profile
+     * @param ShowAction $action
+     * @return JsonResponse
+     */
+    public function show(Profile $profile, ShowAction $action)
+    {
+        return $action($profile->id);
+    }
+
+    /**
+     * @param UpdateRequest $request
+     * @param Profile $profile
+     * @param UpdateAction $action
+     * @return JsonResponse
+     */
+    public function update(UpdateRequest $request, UpdateAction $action)
+    {
+        $profile = auth()->user()->profile;
+        return $action($profile->id, $request->validated());
+    }
+
+}
---
+++ app/Http/Controllers/Api/WorkHistoryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Actions\WorkHistory\DestroyAction;
+use App\Actions\WorkHistory\StoreAction;
+use App\Actions\WorkHistory\UpdateAction;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\WorkHistory\StoreRequest;
+use App\Http\Requests\Api\WorkHistory\UpdateRequest;
+use App\Models\WorkHistory;
+use Illuminate\Http\JsonResponse;
+
+class WorkHistoryController extends Controller
+{
+    /**
+     * Summary of store
+     * @param \App\Http\Requests\Api\WorkHistory\StoreRequest $request
+     * @param \App\Actions\WorkHistory\StoreAction $action
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(StoreRequest $request, StoreAction $action): JsonResponse
+    {
+        return $action($request->validated());
+    }
+
+    /**
+     * Summary of update
+     * @param \App\Http\Requests\Api\WorkHistory\UpdateRequest $request
+     * @param \App\Models\WorkHistory $workHistory
+     * @param \App\Actions\WorkHistory\UpdateAction $action
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(UpdateRequest $request, WorkHistory $workHistory, UpdateAction $action): JsonResponse
+    {
+        return $action($workHistory->id, $request->validated());
+    }
+
+    /**
+     * Summary of destroy
+     * @param \App\Models\WorkHistory $workHistory
+     * @param \App\Actions\WorkHistory\DestroyAction $action
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(WorkHistory $workHistory, DestroyAction $action): JsonResponse
+    {
+        return $action($workHistory->id);
+    }
+}   
